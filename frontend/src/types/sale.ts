5f0a6b2ccf76d36da5b7cfa34684bbb1a4a819f6
Victor Luiz <victorlrosa@hotmail.com>
2021-09-10 23:59:31 -0300

    DataTable Integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,6 @@
+import { NumericLiteral } from "typescript"
+import { Seller } from "./seller"
+
 export type SaleSum = {
     sellerName: string;
     sum: number;
@@ -7,4 +10,25 @@ export type SaleSuccess = {
     sellerName: string;
     visited: number;
     deals: number;
+}
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[]
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    first: boolean;
+    numberOfElements?: number;
+    size?: number;
+    number: number
+    empty?: boolean;
 }
